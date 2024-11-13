d1c215503775812d35c2b1c5bb1c9071316d27ce
Eduardo Gonzalez Melgoza <edu.glz.mlz@gmail.com>
2024-11-13 10:26:52 +0100

    Initial commit

---
+++ GitWorkshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitWorkshop
+//
+//  Created by Eduardo Gonzalez Melgoza on 13/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitWorkshop/GitWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitWorkshopApp.swift
+//  GitWorkshop
+//
+//  Created by Eduardo Gonzalez Melgoza on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
